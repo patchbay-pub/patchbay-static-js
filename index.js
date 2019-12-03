b83db4de61cd26a058f98e867e98a7cf591ea998
Anders Pitman <tapitman11@gmail.com>
2019-12-03 08:31:59 -0700

    Auto index.html serving

--- index.js
+++ index.js
@@ -14,19 +14,38 @@ const rootChannel = process.argv[2];
 
 const srv = http.createServer(async (req, res) => {
 
-  const urlParts = url.parse(req.url);
-
   const rootDir = './';
 
+  const urlParts = url.parse(req.url);
+
   try {
-    const filePath = path.join(rootDir, urlParts.pathname.slice(rootChannel.length));
-    const stats = await fs.promises.stat(filePath);
-    const stream = fs.createReadStream(filePath);
-    stream.pipe(res);
+    const childPath = path.join(rootDir, urlParts.pathname.slice(rootChannel.length));
+    const stats = await fs.promises.stat(childPath);
+
+    if (stats.isFile()) {
+      const stream = fs.createReadStream(childPath);
+      stream.pipe(res);
+    }
+    else {
+
+      const indexPath = path.join(childPath, 'index.html');
+
+      try {
+        const indexStats = await fs.promises.stat(indexPath);
+        const indexStream = fs.createReadStream(indexPath);
+        indexStream.pipe(res);
+      }
+      catch (e) {
+        res.setHeader('Pb-Status', '404');
+        res.write("Not found");
+        res.end();
+      }
+    }
   }
   catch (e) {
     console.error(e);
-    res.write("Fail");
+    res.setHeader('Pb-Status', '404');
+    res.write("Not found");
     res.end();
   }
 });
